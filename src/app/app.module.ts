0942b6e98c7a09483997cf8816a38b0ee4d88032
izrpv <izrpv@ya.ru>
2025-03-19 17:20:33 +0300

    feat: кнопки открытия, редактирования и удаления элемента перемещены в меню

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { MatMenuModule } from '@angular/material/menu';
 
 const materialModules = [
   DragDropModule,
@@ -34,6 +35,7 @@ const materialModules = [
   MatDatepickerModule,
   MatNativeDateModule,
   MatSnackBarModule,
+  MatMenuModule,
 ];
 
 const matDateLocaleProvider = { provide: MAT_DATE_LOCALE, useValue: 'ru-RU' };
